d496c9f41f7ea6381ce52caa87c6d016bf3028c5
xuanwenchao <407300101@qq.com>
2023-04-25 09:54:30 +0800

    添加未签名交易至offchain_worker

--- pallets/ocw-test/src/lib.rs
+++ pallets/ocw-test/src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use frame_system::{
-	offchain::{AppCrypto, CreateSignedTransaction, SendSignedTransaction, Signer},
+	offchain::{AppCrypto, CreateSignedTransaction, SendSignedTransaction, Signer, SubmitTransaction},
 	pallet_prelude::*,
 };
 use sp_core::crypto::KeyTypeId;
@@ -52,36 +52,53 @@ pub mod pallet {
 	// The pallet's runtime storage items.
 	// https://docs.substrate.io/main-docs/build/runtime-storage/
 	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u64>;
+	#[pallet::getter(fn something_signed)]
+	pub type SomethingSigned<T> = StorageValue<_, u64>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/main-docs/build/events-errors/
+	#[pallet::storage]
+	#[pallet::getter(fn something_unsigned)]
+	pub type SomethingUnSigned<T> = StorageValue<_, u64>;
+
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		SendSignedSomething(u64, T::AccountId),
+		SendUnSignedSomething(u64),
 	}
 
-	
+	#[pallet::error]
+	pub enum Error<T> {
+		OffchainunsignedTransactionError,
+	}
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
 		pub fn submit_send_signed_something(
+			//签名交易函数
 			origin: OriginFor<T>,
 			blocknumber: u64,
 		) -> DispatchResult {
-			log::info!("### submit_send_signed_something start...]");
 			let who = ensure_signed(origin)?;
 			log::info!(
 				"### submit_send_signed_something who:[{:?}], number:[{:?}]",
 				who,
 				blocknumber
 			);
-			<Something<T>>::put(blocknumber);
+			<SomethingSigned<T>>::put(blocknumber);
 			Self::deposit_event(Event::SendSignedSomething(blocknumber, who));
+			Ok(())
+		}
+
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn submit_send_unsigned_something(
+			//未签名交易函数
+			origin: OriginFor<T>,
+			blocknumber: u64,
+		) -> DispatchResult {
+			ensure_none(origin)?;
+			<SomethingUnSigned<T>>::put(blocknumber);
+			Self::deposit_event(Event::SendUnSignedSomething(blocknumber));
 			Ok(())
 		}
 	}
@@ -89,11 +106,23 @@ pub mod pallet {
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		fn offchain_worker(block_number: T::BlockNumber) {
-			let signer = Signer::<T, T::AuthorityId>::any_account();
-			log::info!("### offchain_worker signer can sign:[{:?}]", signer.can_sign());
 			let number: u64 = block_number.try_into().unwrap_or(0);
 			log::info!("### offchain_worker signer number:[{:?}]", number);
 
+			//-----------------------------未签名交易函数的调用--------------------------			
+			let call = Call::submit_send_unsigned_something { blocknumber:number };
+			if let Err(e) =
+				SubmitTransaction::<T, Call<T>>::submit_unsigned_transaction(call.into())
+					.map_err(|_| <Error<T>>::OffchainunsignedTransactionError)
+			{
+				log::error!(target:"ocw-test","### offchain_worker submit unsigned transaction error:{:?}",e);
+			} else {
+				log::info!(target:"ocw-test","### offchain_worker submit unsigned transaction successful!");
+			}
+
+			//-----------------------------签名交易函数的调用--------------------------
+			let signer = Signer::<T, T::AuthorityId>::any_account();
+			log::info!("### offchain_worker signer can sign:[{:?}]", signer.can_sign());
 			let result = signer.send_signed_transaction(|_account| {
 				Call::submit_send_signed_something { blocknumber: number }
 			});
@@ -103,14 +132,32 @@ pub mod pallet {
 					if let Err(e) = res {
 						log::info!("### Error! send_signed_transaction:[{:?}]", e);
 					} else {
-                        log::info!("### Error! Unknow100");
-                    }
+						log::info!("### Error! Unknow100");
+					}
 				} else {
-                    log::info!("### send_signed_transaction Successful!");
-                }
-			}else {
-                log::info!("### Error! NoAccountForSigning!");
-            }
+					log::info!("### send_signed_transaction Successful!");
+				}
+			} else {
+				log::info!("### Error! NoAccountForSigning!");
+			}
+		}
+	}
+
+   //需要实现未签名交易的验证trait
+	#[pallet::validate_unsigned]
+	impl<T:Config> ValidateUnsigned for Pallet<T> {
+		type Call = Call<T>;
+
+		fn validate_unsigned(_source:TransactionSource, call:&Self::Call) -> TransactionValidity{
+			if let Call::submit_send_unsigned_something{ blocknumber:_ } = call{
+				ValidTransaction::with_tag_prefix("OcwUnsigtx")
+				.priority(TransactionPriority::max_value())
+				.longevity(1)
+				.propagate(false)
+				.build()
+			} else {
+				InvalidTransaction::Call.into()
+			}
 		}
 	}
 }
